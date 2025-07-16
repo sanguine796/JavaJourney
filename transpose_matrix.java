f01a13c9a9690f4595c9191588b0957f982c4e4a
Sanguine <nvsksanguine@gmail.com>
2025-07-16 19:22:42 +0530

    transpose of matrix

--- transpose_matrix.java
+++ transpose_matrix.java
@@ -1,20 +1,22 @@
-public class transpose_matrix
-{
-    public static void main(String arg[])
-    {
-        int[][] arr1={{1,3,5},{6,7,8}};
-       int n = arr1.length;        
-        int m = arr1[0].length;
-        int[][] result = new int[m][n];
+public class transpose_matrix {
+    public static void main(String arg[]) {
+        int[][] arr1 = {
+            {1, 3, 5},
+            {6, 7, 8}
+        };
 
+        int n = arr1.length;        // rows = 2
+        int m = arr1[0].length;     // cols = 3
+        int[][] result = new int[m][n]; // result is 3x2
 
+        // Transpose logic
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 result[j][i] = arr1[i][j];
             }
         }
 
-
+        // Correct printing for result matrix
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(result[i][j] + " ");
@@ -23,6 +25,7 @@ public class transpose_matrix
         }
     }
 }
+
 
 
 
